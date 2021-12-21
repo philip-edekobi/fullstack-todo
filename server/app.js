6b3b4c907107adcfa56478a9e4d8a5812388168a
poposki317 <philipedekobi@gmail.com>
2021-12-21 18:13:34 +0100

    ui revision

--- server/app.js
+++ server/app.js
@@ -10,8 +10,11 @@ app.use(cors({
     origin: "http://localhost:3000",
     credentials: true,
 }));
+
 app.use("/todos", todoRoutes);
 
 require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URI).then(res => app.listen(5000, () => console.log("server is running on port 5000"))).catch(err => console.log(err));
+mongoose.connect(process.env.MONGO_URI)
+    .then(res => app.listen(5000, () => console.log("server is running on http://localhost:5000/todos")))
+    .catch(err => console.log(err));
