faea8f930c5b080c6b5be8b5be535f7334414950
poposki317 <philipedekobi@gmail.com>
2021-12-27 12:42:19 +0100

    readying for deployment

--- server/app.js
+++ server/app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const routes = require("./routes.js");
 
 const mongoose = require('mongoose');
+const path = require('path');
 
 app.use(express.json());
 
@@ -15,6 +16,11 @@ app.use("/api/", routes);
 require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, "..", "client", "build")));
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
+});
 
 mongoose.connect(process.env.MONGO_URL)
     .then(res => app.listen(PORT, () => console.log(`server is running on http://localhost:${PORT}`)))
